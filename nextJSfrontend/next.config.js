017ac2575287238fc5f33889d95f913c73b5635e
BRMilev22 <BRMilev22@codingburgas.bg>
2025-03-27 18:13:43 +0200

    Add Unsplash domain to Next.js config (Close #21)

--- nextJSfrontend/next.config.js
+++ nextJSfrontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['geist'],
+  images: {
+    domains: ['via.placeholder.com', 'images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig; 
